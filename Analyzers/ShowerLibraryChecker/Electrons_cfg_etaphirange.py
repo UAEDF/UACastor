8feb685e0ece0306bf7c702a016e27319be49c22
hvanhaev <>
2010-09-02 14:56:13 +0000

    upload the correct file

--- Analyzers/ShowerLibraryChecker/Electrons_cfg_etaphirange.py
+++ Analyzers/ShowerLibraryChecker/Electrons_cfg_etaphirange.py
@@ -50,7 +50,7 @@ process.source = cms.Source("EmptySource")
 process.output = cms.OutputModule("PoolOutputModule",
     #splitLevel = cms.untracked.int32(0),
     #outputCommands = process.RAWSIMEventContent.outputCommands, #+cms.untracked.vstring('keep *_simCastorDigis_*_*'),
-    fileName = cms.untracked.string('CastorSim_GEN-SIM_PGun_Electron45GeV_SL_10000events_etaphipoint.root')
+    fileName = cms.untracked.string('CastorSim_GEN-SIM_PGun_Electron5GeV_FullSim_10000events_etaphirange.root')
     #dataset = cms.untracked.PSet(
     #    dataTier = cms.untracked.string('GEN-SIM-RAW'),
     #    filterName = cms.untracked.string('')
@@ -69,12 +69,12 @@ common_pgun_particleID = cms.PSet(
 process.generator = cms.EDProducer("FlatRandomEGunProducer",
     PGunParameters = cms.PSet(
         common_pgun_particleID,
-        MinEta = cms.double(-5.9),
-        MaxEta = cms.double(-5.9),
-        MinPhi = cms.double(0.196),
-        MaxPhi = cms.double(0.196), # PI/4 = 0.7854
-        MinE = cms.double(44.99),
-        MaxE = cms.double(45.01)
+        MinEta = cms.double(-6.6),
+        MaxEta = cms.double(-5.2),
+        MinPhi = cms.double(-3.14159265),
+        MaxPhi = cms.double(3.14159265), # PI/4 = 0.7854
+        MinE = cms.double(4.99),
+        MaxE = cms.double(5.01)
     ),
     AddAntiParticle = cms.bool(False),
     Verbosity = cms.untracked.int32(False)
@@ -143,10 +143,5 @@ process.g4SimHits.SteppingAction = cms.PSet(
 
 
 
-process.g4SimHits.CastorSD.useShowerLibrary = True
+process.g4SimHits.CastorSD.useShowerLibrary = False
 
-process.g4SimHits.CastorSD.minEnergyInGeVforUsingSLibrary = 1.
-process.g4SimHits.CastorShowerLibrary.FileName = 'SimG4CMS/ShowerLibraryProducer/test/python/SL_em+had_E1-1.5-2-2.5-3-3.5-4-4.5-5-6-7-8-9-10-12.5-15-17.5-20-25-30-35-40-45-50GeV_eta-6.0phi0.3_100events-ppON.root'
-process.g4SimHits.CastorShowerLibrary.BranchEvt = 'hadShowerLibInfo.'
-process.g4SimHits.CastorShowerLibrary.BranchEM = 'emParticles.'
-process.g4SimHits.CastorShowerLibrary.BranchHAD = 'hadParticles.'

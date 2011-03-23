5154e026503d6787a6ae706216aa631042d2fe24
roland <>
2011-03-23 14:55:09 +0000

    *** empty log message ***

--- CastorDQM/DQMscript/submit_template.sh
+++ CastorDQM/DQMscript/submit_template.sh
@@ -9,10 +9,9 @@ storedir=STOREDIR
 cfgfile=CFGFILE
 run=RUN
 
-if [ $debug -eq 1 ] ; then echo "farm dir: $farmdir";
+echo "farm dir: $farmdir";
 echo "python dir: $pythondir";
 echo -e "store dir: $storedir\n";
-fi
 
 cd ${pythondir}
 export SCRAM_ARCH=slc5_amd64_gcc434;
@@ -24,10 +23,9 @@ cd ${farmdir}
 logfile=${cfgfile:0:${#cfgfile}-3}.log 
 outputfile=DQM_V0001_Castor_R000$run.root
 
-if [ $debug -eq 1 ] ; then echo "config file: ${cfgfile}";
+echo "config file: ${cfgfile}";
 echo "log file: ${logfile}";
 echo -e "output file: ${outputfile}\n";
-fi
 
 cmsRun $cfgfile > $logfile 2>&1
 return_val=$?

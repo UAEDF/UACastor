e39a22c50d2eec3649ef37e0079455640cae806e
hvanhaev <>
2011-07-27 08:55:43 +0000

    add code to read out filter results and put them in the same HLT object

--- CastorTree/src/GetHLTrig.cc
+++ CastorTree/src/GetHLTrig.cc
@@ -19,7 +19,7 @@
 
 #include "./CastorTree.h"
 
-bool HLTDebug = true;
+bool HLTDebug = false;
 
 void CastorTree::GetHLTrig(const edm::Event& iEvent, const edm::EventSetup& iSetup) {
 
@@ -41,14 +41,19 @@ void CastorTree::GetHLTrig(const edm::Event& iEvent, const edm::EventSetup& iSet
     srcTriggerResults_ = edm::InputTag(srcTriggerResults_.label()+"::"+hltProcName);
   }
 
-  if(HLTDebug) cout<<srcTriggerResults_<<endl;
+  if(HLTDebug) cout<< "The used collection is: " << srcTriggerResults_<<endl;
  
   //-- Fetch HLT Data Object
  
   edm::Handle<edm::TriggerResults> trigResults;
   iEvent.getByLabel(srcTriggerResults_,trigResults);
+	
+	// fetch the collection for the filter results
+	edm::Handle<edm::TriggerResults> filterResults;
+	iEvent.getByLabel(FilterResults_,filterResults);
  
   const edm::TriggerNames& trigNames = iEvent.triggerNames(*trigResults);
+  const edm::TriggerNames& filterNames = iEvent.triggerNames(*filterResults);
  
   //-- Loop on triggers requested by user (config file)
 
@@ -59,6 +64,17 @@ void CastorTree::GetHLTrig(const edm::Event& iEvent, const edm::EventSetup& iSet
 
     if(HLTDebug) cout<<(*hlt_name).c_str()<<" = "<<hasFired(*hlt_name,trigNames,*trigResults)<<endl;
   }
+	
+  // loop again on the Trigger results collection from the filters
+	for(vector<string>::iterator filter_name = filter_bits.begin(); filter_name != filter_bits.end(); filter_name++) {
+		
+		HLTrig.HLTmap[*filter_name]= hasFired(*filter_name,filterNames,*filterResults);
+		HLTrig.HLTprescale[*filter_name]= hltConfig.prescaleValue(iEvent,iSetup,*filter_name);
+		
+		if(HLTDebug) cout<<(*filter_name).c_str()<<" = "<<hasFired(*filter_name,filterNames,*filterResults)<<endl;
+	}
+	
+	
 }
 
 bool CastorTree::hasFired(const std::string& triggerName, const edm::TriggerNames& trigNames, const edm::TriggerResults& trigResults) const {

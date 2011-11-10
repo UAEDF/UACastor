fd00c6d9c845f15b9c9978c205b61fecb43c2223
roland <>
2011-11-10 10:13:16 +0000

    *** empty log message ***

--- CastorTree/Analyzer/Plot.cc
+++ CastorTree/Analyzer/Plot.cc
@@ -18,15 +18,20 @@ int main(int argc, char *argv[]) {
     k++;
   }
 
-  TString title = argv[1];
-  cout<<"I will look in data and MC for the histo with title "<<title.Data()<<endl;
+  TString rootfile = argv[1];
+  TString title = argv[2];
+  TString globalname = argv[3];
+
+  cout<<"I will look in data and MC files "<<rootfile.Data()<<" for the histos with title "<<title.Data()<<endl;
+  cout<<"All canvas will be saved with the global name: "<<globalname.Data()<<endl;
 
   TApplication* rootapp = new TApplication("app",&argc, argv);
 
   MainAnalyzer* m = new MainAnalyzer();
 
-  m->plotHistos("../Result/","output_profile",title);
-  
+  m->plotHistos("../Result/",rootfile,title);
+  m->saveAllCanvas("../Plot/",globalname);
+
   delete m;
   
   return(0);

997b172a704ce3560495b3b694ce99efd0fe7ee3
cbaus <>
2012-05-14 18:39:39 +0000

    some smaller fixes

--- PMTTestAnalysis/src/Plot_All.cxx
+++ PMTTestAnalysis/src/Plot_All.cxx
@@ -1,7 +1,16 @@
 #include <stdio.h>
+#include <string>
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
 
-void Plot_All(string inf, int color, string points){
-  FILE *f = fopen(inf.c_str(), "r");
+#include <TAttFill.h>
+#include <TCanvas.h>
+#include <TGraphErrors.h>
+using namespace std;
+
+void Plot_All(string filename="ExampleFiles/cpt_sf_CA1579_2012_05_13_09_56_20.cpt", int color=kBlue, string points="p"){
+  FILE *f = fopen(filename.c_str(), "r");
   if(f==NULL) {
     std::cout << "can't find file\n";
     exit(1);
@@ -10,12 +19,12 @@ void Plot_All(string inf, int color, string points){
   int imax = 0;
 //  float time, tmp;
   float measure_time, time, hv, cath, adut, aref;
-  bool stop = false;
+    bool stop = false;
   while(!feof(f) && !stop){
 //    fscanf(f, "%f %E %E %E", &time, &tmp, &tmp, &tmp);
     fscanf(f,"%19s %f %f %f %f %f", &measure_time, &time, &hv, &cath, &adut, &aref);
-    if{time > 0}(imin++);
-//    if{time > 1000}(stop = true);
+    if(time > 0){imin++;}
+//    if(time > 1000){stop = true;}
     imax++;
    };
    imax-=2;
@@ -56,23 +65,24 @@ void Plot_All(string inf, int color, string points){
     };
   };
 */
-  for(i=0; i<imax; i++){
+  for(int i=0; i<imax; i++){
     int pos = i;
    fscanf(f,"%19s %f %f %f %f %f", arrmeasure_time+pos, arrtime+pos, arrhv+pos, arrcath+pos, arradut+pos, arraref+pos);
   };
 
   fclose(f);
-
+  
   TGraphErrors * ghv = new TGraphErrors(count, arrtime, arrhv,  0, 0);
-                 ghv->SetTitle("High voltage"); ghv->SetMarkerColor(color);
+  ghv->SetTitle("High voltage"); ghv->SetMarkerColor(color);
   TGraphErrors * gcath = new TGraphErrors(count, arrtime, arrcath, 0, 0);
-                 gcath->SetTitle("Cathode"); gcath->SetMarkerColor(color);
+  gcath->SetTitle("Cathode"); gcath->SetMarkerColor(color);
   TGraphErrors * gadut = new TGraphErrors(count, arrtime, arradut,   0, 0);
-                 gadut->SetTitle("Current DUT"); gadut->SetMarkerColor(color);
+  gadut->SetTitle("Current DUT"); gadut->SetMarkerColor(color);
   TGraphErrors * garef = new TGraphErrors(count, arrtime, arraref,   0, 0);
-                 garef->SetTitle("Current REF"); garef->SetMarkerColor(color);
+  garef->SetTitle("Current REF"); garef->SetMarkerColor(color);
 
-
+  TCanvas * c = NULL;
+  c = new TCanvas("c","",800,600);
   c->cd(1);
   ghv->Draw(points.c_str());
   c->cd(2);

a4db7170766b6fa8e5c389f3251717309fbfced7
cipriano <>
2012-05-21 16:47:08 +0000

    data pack from 15 to 20 may - part2

--- PMTTestAnalysis/src/run_create_tree.C
+++ PMTTestAnalysis/src/run_create_tree.C
@@ -4,7 +4,7 @@ gROOT -> Reset();
 gROOT -> ProcessLine(".L LeakageSubtractor.C++");
 gROOT -> ProcessLine(".L create_tree.C++");
 
-string files_in[20];
+string files_in[30];
 
 files_in[0] = "../data/cpt_sf_CA1579_2012_05_12_15_32_59_mod.cpt";
 files_in[1] = "../data/cpt_sf_CA1579_2012_05_13_09_56_20_mod.cpt";
@@ -15,20 +15,36 @@ files_in[5] = "../data/cpt_sf_CA1842_2012_05_14_17_08_20_mod.cpt";
 files_in[6] = "../data/cpt_sf_CA1592_2012_05_15_08_54_52_mod.cpt";
 files_in[7] = "../data/cpt_sf_CA1592_2012_05_15_12_02_58_mod.cpt";
 files_in[8] = "../data/cpt_sf_CA0327_2012_05_15_15_20_10_mod.cpt";
-files_in[9] = "../data/cpt_sf_CA0540_2012_05_19_22_29_01_mod.cpt";
+files_in[9] = "../data/cpt_sf_CA1680_2012_05_16_07_55_12_mod.cpt";
 files_in[10] = "../data/cpt_sf_CA0558_2012_05_16_17_14_48_mod.cpt";
-files_in[11] = "../data/cpt_sf_CA0683_2012_05_20_12_41_46_mod.cpt";
-files_in[12] = "../data/cpt_sf_CA0812_2012_05_17_09_56_21_mod.cpt";
-files_in[13] = "../data/cpt_sf_CA0857_2012_05_18_23_13_00_mod.cpt";
-files_in[14] = "../data/cpt_sf_CA0886_2012_05_18_14_30_24_mod.cpt";
-files_in[15] = "../data/cpt_sf_CA1321_2012_05_18_01_04_36_mod.cpt";
-files_in[16] = "../data/cpt_sf_CA1537_2012_05_18_09_53_11_mod.cpt";
-files_in[17] = "../data/cpt_sf_CA1617_2012_05_21_00_24_41_mod.cpt";
-files_in[18] = "../data/cpt_sf_CA1620_2012_05_20_15_58_54_mod.cpt";
-files_in[19] = "../data/cpt_sf_CA1680_2012_05_16_07_55_12_mod.cpt";
+files_in[11] = "../data/cpt_sf_CA0812_2012_05_17_09_56_21_mod.cpt";
+files_in[12] = "../data/cpt_sf_CA1954_2012_05_17_15_57_58_mod.cpt";
+files_in[13] = "../data/cpt_sf_CA1321_2012_05_18_01_04_36_mod.cpt";
+files_in[14] = "../data/cpt_sf_CA1537_2012_05_18_09_53_11_mod.cpt";
+files_in[15] = "../data/cpt_sf_CA0886_2012_05_18_14_30_24_mod.cpt";
+files_in[16] = "../data/cpt_sf_CA0857_2012_05_18_23_13_00_mod.cpt";
+files_in[17] = "../data/cpt_sf_CA1832_2012_05_19_08_57_38_mod.cpt";
+files_in[18] = "../data/cpt_sf_CA0540_2012_05_19_22_29_01_mod.cpt";
+files_in[19] = "../data/cpt_sf_CA0683_2012_05_20_12_41_46_mod.cpt";
+files_in[20] = "../data/cpt_sf_CA1620_2012_05_20_15_58_54_mod.cpt";
+files_in[21] = "../data/cpt_sf_CA1829_2012_05_20_21_04_16_mod.cpt";
+files_in[22] = "../data/cpt_sf_CA3362_2012_05_20_09_24_04_mod.cpt";
+files_in[23] = "../data/cpt_sf_CA1617_2012_05_21_00_24_41_mod.cpt";
+files_in[24] = "../data/mod.cpt";
+files_in[25] = "../data/mod.cpt";
+files_in[26] = "../data/mod.cpt";
+files_in[27] = "../data/mod.cpt";
+files_in[28] = "../data/mod.cpt";
+files_in[29] = "../data/mod.cpt";
 
 
-int n_files = 20;
+
+
+
+
+
+
+int n_files = 24;
 
 string tree_out = "test_tree.root";
 

b36236d90b0bc6bc9f731441a0aca9acc0ae6e37
sdooling <>
2012-05-29 18:17:04 +0000

    Leackage Subtrator

--- PMTTestAnalysis/src/LeakageSubtractor.C
+++ PMTTestAnalysis/src/LeakageSubtractor.C
@@ -1,5 +1,6 @@
 #include "TF1.h"
 #include "TGraphErrors.h"
+#include "TGraph.h"
 #include "TFitResult.h"
 #include "TFitResultPtr.h"
 #include "TCanvas.h"
@@ -14,11 +15,21 @@
 #include "TROOT.h"
 #include "TSystem.h"
 #include "TStyle.h"
+#include "TVirtualFitter.h"
+#include "TMinuit.h"
+
+// fit function
+
+double fexp(double *x, double *p){
+ // if(p[1]<0) return 0;
+    return p[0] + p[1] * exp (-(x[0]/p[2] ) );
+};
 
 void LeakageSubtractor(const std::vector<int> &time, std::vector<float> &cathode, const std::vector<int> &hv, const std::vector<int> &led){
 
+gStyle->SetOptFit(1);
 int skipAtVChange = 40;
-int skipAtLedChange = 3;
+int skipAtLedChange = 4;
 
 int size = 0;
 
@@ -26,7 +37,7 @@ std::vector<float> cathode_ye;
 
 int voltage; 
 int voltageBegin = 0;
-int voltageStep = 0; //the lenght of the high voltage steps //don't look at first value because picoamp meter screws this up
+//int voltageStep = 0; //the lenght of the high voltage steps //don't look at first value because picoamp meter screws this up
 
 float error = (0.5/16.6)*10E-12;
 
@@ -34,6 +45,7 @@ size = cathode.size(); //the number pf voltages stored in the cathode vector
 
 //declare the values of the voltage to fit
 std::vector<double> fitX;
+std::vector<double> fitXe;
 std::vector<double> fitY;
 std::vector<double> fitYe;
 
@@ -48,35 +60,114 @@ std::vector<double> fitYe;
   int nstep = 7;
   int step[7]={0,800,900,1000,1200,1400,1600}; 
   //write the endex of the beginning and the end of the hv regions into an array
+  int voltageStep[nstep];
   int index_begin[nstep];
   int index_end[nstep];
 
   
-   for(int istep = 0; istep < 6; istep++){
-  int index1 =0;
-  int index2 =0;
+   for(int istep = 0; istep < 7; istep++){
+    int index1 =0;
+    int index2 =0;
     while(abs(hv[index1]) < step[istep] +20 ){
-	index1++;
-     }
-	index_begin[istep]= index1;
-	cout << "index_begin " <<index_begin[istep] << endl;
+      index1++;
+    }
+    index_begin[istep]= index1;
+    cout << "index_begin " <<index_begin[istep] << endl;
     while(abs(hv[index2]) < step[istep+1] +20 ){
-	index2++;
-     }
-	index_end[istep] =index2;
-	cout << "index_end " << index_end[istep] << endl;
-   }
-   //check that led was turned off
+      index2++;
+    }
+    index_end[istep] =index2;
+    cout << "index_end " << index_end[istep] << endl;
 
+    voltageStep[istep] = index_end[istep] - index_begin[istep];
+    cout << "voltage Step " << voltageStep[istep] << endl;
+}
    //write the voltages into the vector
    // copy the x,y values for one voltage set and only where LED was off 
-	for(int i = index_begin[0]; i < index_end[0]; i++){         
+           
+    for(int i = index_begin[0]; i < index_end[0]; i++){  
+     if(led[i] == 0 && led[i-skipAtLedChange] ==0 && led[i+skipAtLedChange] == 0){       
           fitX.push_back(time[i]-time[index_begin[0]]); 
-	  fitY.push_back(cathode[i]*1e12);
-          fitYe.push_back(cathode_ye[i]*1e12);
-	}
+	  fitY.push_back(cathode[i]);
+          fitYe.push_back(cathode_ye[i]);
+          fitXe.push_back(0.);
+    }
+   }
+	cout << "========" << hv[index_begin[0]] << "===========" << endl;
 
-  // ----loop over all voltage regions----
+   //do the fitting
+   // define the fit function
+   int shift =0;
+   
+   // TGraphErrors has to be defined befor TF1 (the fit function)
+   TGraphErrors *gc0 = new TGraphErrors(fitX.size(),&fitX.front(),&fitY.front(),NULL,&fitYe.front());
+
+   TF1 *ff  = new TF1("ff", fexp, index_begin[0]+shift, index_end[0], 3);
+	ff->SetLineColor(2);
+    int fcount=0;
+    bool repeate=true;
+    double ini1[5] = {1.0e-7, 0.1e-7, 10.e-7, 0.01e-7, 100.e-7};
+   //    double ini1[5] = {1.0e-9, 0.1e-9, 10.e-9, 0.01e-9, 100.e-9};
+    TVirtualFitter::SetMaxIterations(7000);
+double pp[3];
+    while (repeate && fcount<5){
+
+
+      ff->SetParameter(0, 0.01e-9);
+      ff->SetParameter(2, 300.);
+
+      ff->SetParameter(1, ini1[fcount]);
+      cout << "ini par[1] = " << ini1[fcount] << endl;
+cout << ff->GetParameter(0) <<" " << ff->GetParameter(1)<< endl;
+      gc0->Fit("ff","ER");
+cout << ff->GetParameter(0) <<" " << ff->GetParameter(1)<< endl;
+	ff->GetParameters(pp);
+cout << pp[0] << " " << pp[1] << endl;
+      cout << "=> " << gMinuit->fCstatu.Data() << endl;    
+      repeate = ( (gMinuit->fCstatu.Data()[0]!='S') || (ff->GetParameter(1)<0) || (ff->GetParameter(0)<0));
+	cout << "repeat " << repeate << " " << gMinuit->fCstatu.Data()[0]<< endl;
+	
+      fcount++;
+     }
+
+    /*  TF1 theFcn("a","[0]+[1]*TMath::Exp(-(x)/[2])");
+      theFcn.SetParameters(fitY.back(),40,60,fabs(voltage-800)<20.?650:350);
+      theFcn.SetParLimits(3,200.,1200.);
+      TGraphErrors theGraph (fitX.size(),&fitX.front(),&fitY.front(),NULL,&fitYe.front());
+
+ // TFitResultPtr theMin = theGraph.Fit(&theFcn,fVerbosity>1?"S M E 0":"S M E 0 Q");
+  //   TFitResultPtr theMin = theGraph.Fit(&theFcn); //do the fit and print out the fit parameters
+      if (theMin == NULL || !theMin->Status() || theMin->IsValid() == 0)
+        {
+          std::cerr << "fit unsucessful" << std::endl;
+        }
+      else
+        {
+          std::cerr << "fit sucessful with f=" << theMin->Chi2()/double(fitX.size()) << std::endl;
+          if (theMin->Chi2()/double(fitX.size()) > 50)
+            std::cout << " - Warning: fit converged but high chi2/ndf compromised: " << theMin->Chi2()/double(fitX.size()) << std::endl;
+        }
+*/
+      	TCanvas * c = new TCanvas("c","c",800,600);
+      	c->cd();
+      //  gc0.SetLineColor(kRed);
+      	gc0->Draw("AP");
+//      theFcn.SetParameters(theMin->Parameter(0),theMin->Parameter(1),theMin->Parameter(2),theMin->Parameter(3));
+     // theFcn.Draw("lSAME");
+      //cin >> fVerbosity;
+      c->Print("example.eps");
+
+ /*     for (int j=0; j < index_end[0]-index_begin[0]; j++)
+        {
+            std::cout << "x:" << time[j+index_begin[0]] << "/" << time[index_begin[0]+j]-time[index_begin[0]] << " ===  " << cathode[j+index_begin[0]] << " ..... " << theFcn(time[index_begin[0]+j]-time[index_begin[0]]) << std::endl;
+          // if any poles in the function just skip the subtraction. only the LED regions are of interest. but if the profile doesn't look nice one can delete this if
+          if ((theFcn)(time[j+index_begin[0]]-time[index_begin[0]]) < 1e-6 && (theFcn)(time[j+index_begin[0]]-time[index_begin[0]]) > 0)
+            cathode[j+index_begin[0]] -= float( theFcn(time[j+index_begin[0]]-time[index_begin[0]]));
+        }//subtract the leakage
+*/
+
+//from Colin
+/*  // ----loop over all voltage regions----
   while (voltageBegin < size)
     {
       // ----Skip 0 voltage run----
@@ -183,5 +274,5 @@ std::vector<double> fitYe;
 
       voltageBegin += voltageStep + 1; //jump to next voltage
      }
-
+*/
 }
